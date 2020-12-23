fb94e28edd16267a3f005d0a98394f16996dc7e0
nabin <devilmeh5@gmail.com>
2020-12-23 23:18:14 +0545

    intitial xcodeproject creation

---
+++ youtube_songs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube_songs
+//
+//  Created by nabin magar on 23/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ youtube_songs/youtube_songsApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_songsApp.swift
+//  youtube_songs
+//
+//  Created by nabin magar on 23/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct youtube_songsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
